b8fff7a7ed0fa40007e35348d3737bdb39f3f94f
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-09-19 17:12:36 +0200

    refactor: move class WordFrequencyCounter to separate file

---
+++ WordCounter.Model.Tests/WordFrequencyCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WordCounter.Model.Tests
+{
+    public class WordFrequencyCounter
+    {
+        public WordFrequencyCounter() { }
+
+        public Dictionary<string, int> CountFrequency(string text)
+        {
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+            List<string> words = SplitTextToWords(RemoveUnwantedCharacters(text));
+
+            foreach (string word in words)
+            {
+                if (!wordFrequency.ContainsKey(word))
+                    wordFrequency.Add(word, words.FindAll(x => word == x).Count);
+            }
+
+            return wordFrequency;
+        }
+
+        private List<string> SplitTextToWords(string text)
+        {
+            return new List<string>(text.Split(new string[] { " ", "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        private string RemoveUnwantedCharacters(string text)
+        {
+            return Regex.Replace(text, "[^A-Za-z\r\n ]", "").ToLower();
+        }
+    }
+}
--- WordCounter.Model.Tests/WordFrequencyCounterTests.cs
+++ WordCounter.Model.Tests/WordFrequencyCounterTests.cs
@@ -1,7 +1,5 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
-using System;
-using System.Text.RegularExpressions;
 
 namespace WordCounter.Model.Tests
 {
@@ -67,34 +65,5 @@ namespace WordCounter.Model.Tests
 
             CollectionAssert.AreEqual(expected, output);
         }
-    }
-
-    public class WordFrequencyCounter
-    {
-        public WordFrequencyCounter() { }
-
-        public Dictionary<string, int> CountFrequency(string text)
-        {
-            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
-            List<string> words = SplitTextToWords(RemoveUnwantedCharacters(text));
-
-            foreach (string word in words)
-            {
-                if (!wordFrequency.ContainsKey(word))
-                    wordFrequency.Add(word, words.FindAll(x => word == x).Count);
-            }
-
-            return wordFrequency;
-        }
-
-        private List<string> SplitTextToWords(string text)
-        {
-            return new List<string>(text.Split(new string[] { " ", "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries));
-        }
-
-        private string RemoveUnwantedCharacters(string text)
-        {
-            return Regex.Replace(text, "[^A-Za-z\r\n ]", "").ToLower();
-        }
-    }
+    }    
 }
