e0ca67e5173658b411250d590e4a7cf30b696409
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-09-20 10:35:24 +0200

    chore: move file WordFrequencyCounter.cs to project Model

--- WordCounter.Model.Tests/WordFrequencyCounterTests.cs
+++ WordCounter.Model.Tests/WordFrequencyCounterTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using WordCounter.Model;
 
 namespace WordCounter.Model.Tests
 {
--- WordCounter.Model/WordFrequencyCounter.cs
+++ WordCounter.Model/WordFrequencyCounter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace WordCounter.Model.Tests
+namespace WordCounter.Model
 {
     public class WordFrequencyCounter
     {
