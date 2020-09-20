2a0951f45de5ce2abab028b6208a3fff577cce35
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-09-20 16:22:25 +0200

    refactor: add rest of views

---
+++ WordCounter.Presentation/MainPresenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter.Presentation
+{
+    public class MainPresenter
+    {
+        private readonly ITextInputView textInputView;
+
+    }
+}
